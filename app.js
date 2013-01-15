a6b0ef012994510b718ad593d65ec290b61e541d
Kamil Cholewiński <harry666t@gmail.com>
2013-01-15 22:41:43 +0100

    Serve Funky Lisp

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 
 var http    = require("http");
+var fs      = require("fs");
 var miniapp = require("./miniapp");
 
 miniapp.route("GET", "/", function (request) {
     return {
-        text: "Hello!",
+        html: fs.readFileSync("fl.html"),
+    };
+});
+
+miniapp.route("GET", "/style.css", function (request) {
+    return {
+        data: fs.readFileSync("style.css"),
+        type: "text/css",
+    };
+});
+
+miniapp.route("GET", "/fl.js", function (request) {
+    return {
+        data: fs.readFileSync("fl.js"),
+        type: "text/javascript",
     };
 });
 
